334466c5806deafe1228eac973e37b0044522730
Malex2299 <stmdelc5@upana.edu.gt>
2023-03-25 03:04:22 -0600

    Se realizó la matriz dificil

--- Busca_minas/Busca_minas/Form4.Designer.cs
+++ Busca_minas/Busca_minas/Form4.Designer.cs
@@ -29,11 +29,128 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inicioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inicioToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(846, 40);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inicioToolStripMenuItem
+            // 
+            this.inicioToolStripMenuItem.Name = "inicioToolStripMenuItem";
+            this.inicioToolStripMenuItem.Size = new System.Drawing.Size(91, 36);
+            this.inicioToolStripMenuItem.Text = "Inicio";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(79, 36);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 40);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(846, 59);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(46, 53);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Showcard Gothic", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(146, 53);
+            this.toolStripLabel1.Text = "Minas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(637, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 53);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tiempo";
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(846, 917);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form4";
             this.Text = "Form4";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem inicioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Busca_minas/Busca_minas/Form4.cs
+++ Busca_minas/Busca_minas/Form4.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,150 @@ namespace Busca_minas
 {
     public partial class Form4 : Form
     {
+        private int cronometro; //variable para el conometro
+        int[] matriz_e = new int[226];// matriz de estado
+        Random Randoms = new Random();
+        int[] minas_pos = new int[45];// posiciones de las minas mn
+        int[] calcular_area_x = new int[8] { -1, 0, 1, 1, 1, 0, -1, -1 };// calcular el area donde se encuentra la bomba
+        int[] calcular_area_y = new int[8] { -1, -1, -1, 0, 1, 1, 1, 0 };// y
         public Form4()
         {
             InitializeComponent();
+            cronometro = 0;
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            //Creación de la matriz
+            int iteracion = 55;
+            int numeroma = 1;
+
+            for (int x = 1; x <= 15; x++) 
+            {
+                int sumax = 1;
+                for (int j = 1; j <= 15; j++)
+                {
+                    Button miboton = new Button();
+                    miboton.Location = new Point(sumax, iteracion);
+                    miboton.Size = new Size(28, 28);
+                    miboton.Name = x + ",";
+                    miboton.TabIndex = numeroma;
+                    miboton.Click += new EventHandler(miBoton_Click);
+                    this.Controls.Add(miboton);
+                    sumax = sumax + 28;
+                    numeroma = numeroma + 1;
+                    miboton.MouseDown += new MouseEventHandler(Form4_MouseDown);
+                }
+
+                iteracion = iteracion + 28;
+            }
+            minas();
+            area();
+
+            Random rnd = new Random();
+
+        }
+
+        private void miBoton_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+
+            int t = ActiveControl.TabIndex;
+            if (matriz_e[t] != 9)
+                ActiveControl.Text = Convert.ToString(matriz_e[t]);
+            else
+                foreach (System.Windows.Forms.Control ctrl in this.Controls)
+                {
+                    int ta = ctrl.TabIndex;
+                    if (matriz_e[ta] == 9)
+                    {
+                        ctrl.ForeColor = Color.Red;
+                        ctrl.Text = "💣";
+                        timer1.Enabled = false;
+                        //SoundPlayer Player = new SoundPlayer();
+                        //Player.SoundLocation = "C:/Users/patri/OneDrive/Desktop/Busca_minas/Busca_minas/Busca_minas/musica/Bomba_1.wav";
+                        //Player.Play();
+                    }
+                }
+
+        }
+
+
+
+        private void minas()
+        {
+            int recorrido, posición, temporal;// posición recorrido a, posición u, temporal t
+
+            for (int minita = 0; minita < 226; minita++) matriz_e[minita] = minita;
+
+            for (posición = 224; posición > 0; posición--)
+            {
+                recorrido = Randoms.Next(posición);
+                temporal = matriz_e[recorrido];
+                matriz_e[recorrido] = matriz_e[posición];
+                matriz_e[posición] = temporal;
+            }
+
+            for (int minita = 0; minita < 45; minita++) minas_pos[minita] = matriz_e[minita];
+
+            for (int minita = 0; minita < 226; minita++) matriz_e[minita] = 0;
+
+            for (int minita = 0; minita < 45; minita++) matriz_e[minas_pos[minita]] = 9;
+        }
+
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void area()
+        {
+            int minas_encontradas, filas, columnas_pos; // minas enncontradas en el area mv, filas f, columnas_pos c
+            int minas_x, minas_y, minas_posicion; // minas_x vx, minas_y vy, minas_posicion pv
+
+            for (int t = 0; t < 226; t++)
+            {
+                if (matriz_e[t] != 9)
+                {
+                    minas_encontradas = 0;
+                    filas = t / 15;
+                    columnas_pos = t % 15;
+
+                    for (int v = 0; v < 8; v++)
+                    {
+
+                        minas_x = filas + calcular_area_x[v];
+                        minas_y = columnas_pos + calcular_area_y[v];
+                        minas_posicion = 15 * minas_x + minas_y;
+                        if (minas_x > -1 && minas_x < 15 && minas_y > 0 && minas_y < 16 && matriz_e[minas_posicion] == 9) minas_encontradas++;
+                    }
+                    matriz_e[t] = minas_encontradas;
+                }
+
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (System.Windows.Forms.Control ctrl in this.Controls) ctrl.Text = (Convert.ToString(matriz_e[ctrl.TabIndex]));
+
+        }
+
+        private void Form4_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                Button currentButton = (Button)sender;
+                currentButton.Text = "🚩";
+            }
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            cronometro++;
+            label2.Text = cronometro.ToString(); //cronometro
         }
     }
 }
